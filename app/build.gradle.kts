b2447beec678b8a653f19c3a288bb1c8f1d0559b
ezequiel-urcino <ezeurcino18@gmail.com>
2025-10-24 10:41:08 -0400

    Full Project Complete

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.hide_and_seek"
-        minSdk = 35
+        minSdk = 34
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
